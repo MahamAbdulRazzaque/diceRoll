b986eb1ce3f00947744693dac7a0392fd32d7e64
Maham Abdul Razzaque <mahamabdulrazzaque@gmail.com>
2024-11-08 17:06:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// //Math.round
+// document.write( "<br>" , Math.round(-5.9));
+// //Math.ceil
+// document.write( "<br>" , Math.ceil(-5.9));
+// //Math.floor   //ignore krta hai decimal num ko
+// document.write( "<br>" , Math.floor(-7.9)); 
+// //Math.random
+// document.write(Math.random());
+
+
+// let diceRoll = Math.floor(Math.random()  *  5) + 1;
+// document.write("Dice Roll:   ", diceRoll);
+
+// let coinToss = Math.round() <0.5 ? "Heads" : "Tails";
+// document.write(coinToss);
+
+
+// let coinToss = Math.random();
+// // document.write(coinToss);
+// if (coinToss < 0.5) {
+//     document.write("Heads");
+// }
+// else{
+//     document.write("Tails");
+// }
+
+
+// let colors = ["purple" , "skyblue" , "black" , "white"];
+// let randomColor = colors[Math.floor(Math.random() * colors.length)];
+// document.write(randomColor);
+
+
